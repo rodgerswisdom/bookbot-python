d9e8bc4cb24f95296fc67baeac88c51740338961
rodgerswisdom <trulyhawona@gmail.com>
2025-03-18 23:40:52 +0300

    Automated commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def count(FILE_PATH):
+    file = with open("FILE_ATH", "r"):
+        content = file.read()
+        print(content)
+
+def main():
+    FILE_PATH = "books/frankeinstein.txt"
+    count(FILE_PATH);
+
+if name = __main__():
+    main()
