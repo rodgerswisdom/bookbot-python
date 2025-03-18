d312ef77c183f1a97f72f0914eb9b13dcdaa1105
rodgerswisdom <trulyhawona@gmail.com>
2025-03-18 23:56:21 +0300

    Automated commit

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 def count(FILE_PATH):
     with open(FILE_PATH, "r") as file:
         content = file.read()
-        print(content)
+        num_words = len(content.split())
+        print(f"{num_words} words")
 
 def main():
     FILE_PATH = "books/frankeinstein.txt"
