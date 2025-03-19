d40460064433a7a226c35a0f79dadb7ec40a2be9
rodgerswisdom <trulyhawona@gmail.com>
2025-03-19 22:39:08 +0300

    Automated commit

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
-from stats import count
+from stats import *
 
 def main():
     FILE_PATH = "books/frankeinstein.txt"
-    count(FILE_PATH)
+
+    text = count(FILE_PATH)
+    word_count = len(text)
+    count_text = count_list(text)
+
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for items in count_text:
+        if items["key"].isalpha():
+            print(f"{items["key"]}:{items["value"]}")
 
 if __name__ ==  "__main__":
     main()
--- stats.py
+++ stats.py
@@ -4,6 +4,21 @@ def count(FILE_PATH):
     with open(FILE_PATH, "r") as file:
         content = file.read()
         num_words = len(content.split())
-        print(f"{num_words} words")
 
+        text = content.lower()
+        word_dict = {}
+
+        for char in text:
+            if char in word_dict:
+                word_dict[char] += 1
+            else:
+                word_dict[char] = 1
+
+        return word_dict
+
+
+def count_list(word_dict):
+    str_list = [{"key":key,"value":value} for key,value in word_dict.items()]
+    str_list = sorted(str_list, key=lambda x:x["value"], reverse=True)
+    return str_list
 
